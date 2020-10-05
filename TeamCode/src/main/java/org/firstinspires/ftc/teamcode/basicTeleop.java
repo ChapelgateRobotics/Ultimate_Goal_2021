c9921988f9c0b0a8035b1e9c9f26ec21f55376fb
ChapelgateRobotics <chapelgategobotics@gmail.com>
2020-10-04 21:40:04 -0400

    Added motor behavior and collector motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicTeleop.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -18,16 +19,34 @@ class basicTeleop extends OpMode
     DcMotor backRight;
     DcMotor frontLeft;
     DcMotor frontRight;
+    RevRoboticsCoreHexMotor collector;
 
     @Override
     public void init()
     {
-        backLeft  = hardwareMap.get(DcMotor.class, "blackLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        // maps motor names to hardware on Rev Expansion Hub
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight= hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft  = hardwareMap.get(DcMotor.class, "blackLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+
+        // sets motors directions
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        //RevRoboticsCore Hex Motor map hardware on Rev Expansion Hub
+        collector=(RevRoboticsCoreHexMotor) hardwareMap.dcMotor.get("collector");
+
+        //set motors power behaviors
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
     }
 
 
